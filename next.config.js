aff0649275a991d8f202ade5ade6249f7f72fa84
Sydney Cole <sydneycole583@gmail.com>
2026-09-21 12:19:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    
+      typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
     remotePatterns: [
       {
@@ -12,3 +17,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
